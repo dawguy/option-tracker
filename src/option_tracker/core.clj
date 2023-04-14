18a2e799da95be44ec6151ed358c020a4927aee9
dawguy <wright.david.27@gmail.com>
2023-04-14 10:20:49 -0400

    Project saves data to CSV files.

--- src/option_tracker/core.clj
+++ src/option_tracker/core.clj
@@ -1,8 +1,20 @@
 (ns option-tracker.core
   (:require [net.cgrand.enlive-html :as html]
             [etaoin.api :as e]
-            [etaoin.keys :as k])
-  (:import (java.io ByteArrayInputStream)))
+            [clojure.data.csv :as csv]
+            )
+  (:import (java.io ByteArrayInputStream)
+           ))
+
+(defn data-dir [] "/tmp/OptionsData/")
+
+(defn save-data [stock-name data date type] "TMUS (:calls {...}) call"
+  (let [today-date (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (java.util.Date.))
+        file-name (str (#'data-dir) stock-name "/" today-date "/" stock-name "-" date "-" type ".csv")]
+    (clojure.java.io/make-parents file-name)
+    (with-open [w (clojure.java.io/writer file-name)]
+      (csv/write-csv w data)
+      )))
 
 ; https://stackoverflow.com/questions/38283891/how-to-wrap-a-string-in-an-input-stream
 (defn string->stream
@@ -14,6 +26,7 @@
 
 (def a-page (atom {}))
 (defn get-html [driver url]
+  (prn "Getting data from " url)
   (if (e/running? driver)
     (do
       (e/go driver url)
@@ -59,7 +72,9 @@
     )))
 (defn date-option->map [option]
   {:value (first (html/attr-values option :value))
-   :date (html/text option)})
+   :date (clojure.string/replace (clojure.string/replace (html/text option) " " "-") "," "")})
+(defn data->csv-format [d]
+  (conj (map (fn [row] (map second row)) d) (vec (map #(str (symbol (first %))) (first d)))))
 (defn page->data [p]
   (let [call-trs (html/select  p [:table.calls :tbody :tr])
         put-trs (html/select  p [:table.puts :tbody :tr])
@@ -83,7 +98,22 @@
                             (reset! a-pages pages)
                             (reset! a-search-stock stock)
                             pages)))
+(defn save-options [stock-name]
+  (let [pages (get-pages stock-name)
+        m-date (reduce #(assoc %1 (:value %2) (:date %2)) {} (:available-dates (second (first pages))))]
+    (loop [[p & rem] pages]
+      (prn (first p))
+      (if (nil? p)
+        true
+        (let [page-data (second p)]
+          (save-data stock-name (data->csv-format (:calls page-data)) (get m-date (first p)) "calls")
+          (save-data stock-name (data->csv-format (:puts page-data)) (get m-date (first p)) "puts")
+          (recur rem))))))
 
-(get-pages "TMUS")
-(prn (str "Loaded options data for " @a-search-stock " " (keys @a-pages)))
-(page->data @a-p)
+(comment ""
+  (save-options "BBBY")
+  (save-options "GME")
+  (save-options "T")
+  (save-options "TMUS")
+  (save-options "VZ")
+,)
