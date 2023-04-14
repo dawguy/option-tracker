5e645975e8b0543277b0508f37ef1dca0b7b2768
dawguy <wright.david.27@gmail.com>
2023-04-14 10:20:49 -0400

    Headless chrome for data fetching

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [clj-http "3.12.3"]
                  [enlive "1.1.6"]
                  [etaoin "1.0.39"]]
   :repl-options {:init-ns option-tracker.core})
